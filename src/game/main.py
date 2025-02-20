d68d9bda0bad7bcf1f9b489a38c54c2b6f4af449
jgreenhoe <greenhoejon@gmail.com>
2025-02-20 10:07:10 -0500

    setting up tk

--- src/game/main.py
+++ src/game/main.py
@@ -3,9 +3,10 @@ import tkinter as tk
 root = tk.Tk()
 root.title("stock market game")
 root.attributes("-fullscreen", True)
+root.update()
+win_size = [root.winfo_width(), root.winfo_height()]
 
-a = tk.Label(root, text="hello")
-a.pack()
-print("a")
+a = tk.Label(root, text="hello", bg="lightblue",font=("arial",90))
+a.grid(row=0, column=0, padx=5, pady=5)
 
 tk.mainloop()
