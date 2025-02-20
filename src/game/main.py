51ddb1f8b95441f94dea3c87c805de5d39ff9890
jgreenhoe <greenhoejon@gmail.com>
2025-02-19 21:56:42 -0500

    Initial commit

---
+++ src/game/main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("stock market game")
+root.attributes("-fullscreen", True)
+
+a = tk.Label(root, text="hello")
+a.pack()
+print("a")
+
+tk.mainloop()
