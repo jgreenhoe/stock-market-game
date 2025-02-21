17bb69bf9c9d6f178518eaace2ddb48c7e4f860e
jgreenhoe <greenhoejon@gmail.com>
2025-02-21 16:12:35 -0500

    improved clock

--- src/game/main.py
+++ src/game/main.py
@@ -8,18 +8,21 @@ root.attributes("-fullscreen", True)
 root.update()
 win_size = [root.winfo_width(), root.winfo_height()]
 
-a = tk.Label(root, text="hello", bg="lightblue",font=("arial",90))
+a = tk.Label(root, text="hello", font=("arial",20))
 a.grid(row=0, column=0, padx=5, pady=5)
 
 #clock = hour.minute
 clock = 0
 filler_zero = ""
-for i in range(48):
-    clock += .25
-    if clock % 1 == 0:
+#number of increments per hour
+interval = 20
+for i in range(interval * 24):
+    clock += 1 / interval
+    #adds a 0 to the display for single-digit minutes
+    if clock % 1 < .1666:
         filler_zero = 0
     else:
         filler_zero = ""
-    a.config(text = f"{math.floor(clock)}:{filler_zero}{round(clock % 1 *60)}")
+    a.config(text = f"{math.floor(clock)}:{filler_zero}{round(clock % 1 * 60)}")
     root.update()
-    time.sleep(1)
+    time.sleep(.5)
