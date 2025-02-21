f1c64397e52fac63913749083fce9bc1bf62e55b
jgreenhoe <greenhoejon@gmail.com>
2025-02-21 10:23:09 -0500

    added clock

--- src/game/main.py
+++ src/game/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 
 root = tk.Tk()
 root.title("stock market game")
@@ -9,4 +10,14 @@ win_size = [root.winfo_width(), root.winfo_height()]
 a = tk.Label(root, text="hello", bg="lightblue",font=("arial",90))
 a.grid(row=0, column=0, padx=5, pady=5)
 
-tk.mainloop()
+#clock = [hour, minute]
+clock = [0,0]
+for i in range(48):
+    if clock[1] == 1:
+        clock[0] += 1
+        clock[1] = 00
+    else:
+        clock[1] = 1
+    a.config(text = f"{clock[0]}:{clock[1]*30}")
+    root.update()
+    time.sleep(1)
