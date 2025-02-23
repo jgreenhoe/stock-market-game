b60c319160f220e407ee4c12c0c4c577d1c95ad7
jgreenhoe <greenhoejon@gmail.com>
2025-02-23 12:23:16 -0500

    improved intraday data

--- src/game/main.py
+++ src/game/main.py
@@ -16,13 +16,17 @@ clock = 0
 filler_zero = ""
 #number of increments per hour
 interval = 6
-for i in range(interval * 24):
-    clock += 1 / interval
-    #adds a 0 to the display for single-digit minutes
-    if clock % 1 < .1666:
-        filler_zero = 0
-    else:
-        filler_zero = ""
-    a.config(text = f"{math.floor(clock)}:{filler_zero}{round(clock % 1 * 60)}")
-    root.update()
-    time.sleep(.5)
+while(1):
+    for i in range(interval * 24):
+        #rounds clock when it should be an integer to avoid floating point inaccuracy
+        if i % interval == 0:
+            clock = round(clock)
+        #adds a 0 to the display for single-digit minutes
+        if clock % 1 < .1666:
+            filler_zero = 0
+        else:
+            filler_zero = ""
+        a.config(text = f"{math.floor(clock)}:{filler_zero}{round(clock % 1 * 60)} {clock}")
+        root.update()
+        clock += 1 / interval
+        time.sleep(1)
